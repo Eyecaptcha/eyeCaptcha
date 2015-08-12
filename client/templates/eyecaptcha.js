9707a5912cf0dd67dfd8680a62428f8442a7c06c
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-08-12 19:47:52 -0400

    changed functions in eyecaptcha.js to push image paths into captchaArray

--- client/templates/eyecaptcha.js
+++ client/templates/eyecaptcha.js
@@ -19,6 +19,8 @@ Template.eyecaptchaContainer.events({
 	}
 });
 
+// Declare captchaArray variable to be used later
+var captchaArray = [];
 
 // Generate random number function
 function randomIntFromInterval(min,max) {
@@ -37,14 +39,15 @@ for( i = 0; i < freeImages; i++){
   function randomImg(){
     var randomNumber = Math.floor(Math.random() * 149) + 1;
     var imgName = "img" + randomNumber;
-    document.getElementById("imageid").src = img/Free + "/" + imgName;
+    var imgPath = "img/Free/" + imgName;
+    captchaArray.push(imgPath);
   }
 };
 
 // Randomly select a prompt to choose from
 var prompts = ["Select the images of the sustainably grown artisanal bouquets now available from Bouqs.",
 "Select the images of the soft drink filled with happiness, now available for only $0.99.",
-"Select the images of the footwear brand that gives you the WILL TO WIN!",
+"JUST DO IT! Select the images of the footwear brand that gives you the WILL TO WIN!",
 "Select the images of the backpack that can take you anywhere, now available from Patagonia.",
 "Hungry? Select the images of the candy bar that satisfies your craving.",
 "Never stop exploring. Select the images of the jacket that you can take on any expedition, now available from North Face."]
@@ -53,41 +56,51 @@ var randomPrompt = prompts[Math.floor(Math.random() * prompts.length)];
 
 // Generate the correct amount of random images from the paid images
 for( i = 0; i < adImages; i++){
-  if(randomPrompt === prompts[0]){
+  if(randomPrompt === prompts[0]) {
     function randomImg(){
       var randomNumber = Math.floor(Math.random() * 11) + 1;
       var imgName = "bouqs" + randomNumber;
-      document.getElementById("imageid").src = img/Paid/Bouqs + "/" + imgName;
+      var paidImgPath = "img/Paid/Bouqs/" + imgName;
+      captchaArray.push(paidImgPath);
     }
   } else if (randomPrompt === prompts[1]) {
     function randomImg(){
       var randomNumber = Math.floor(Math.random() * 10) + 1;
       var imgName = "coke" + randomNumber;
-      document.getElementById("imageid").src = img/Paid/CocaCola + "/" + imgName;
+      var paidImgPath = "img/Paid/CocaCola/" + imgName;
+      captchaArray.push(paidImgPath);
     }
   } else if (randomPrompt === prompts[2]) {
     function randomImg(){
       var randomNumber = Math.floor(Math.random() * 10) + 1;
       var imgName = "nike" + randomNumber;
-      document.getElementById("imageid").src = img/Paid/Nike + "/" + imgName;
+      var paidImgPath = "img/Paid/Nike/" + imgName;
+      captchaArray.push(paidImgPath);
     }
   } else if (randomPrompt === prompts[3]) {
     function randomImg(){
       var randomNumber = Math.floor(Math.random() * 10) + 1;
       var imgName = "patagonia" + randomNumber;
-      document.getElementById("imageid").src = img/Paid/Patagonia + "/" + imgName;
+      var paidImgPath = "img/Paid/Patagonia/" + imgName;
+      captchaArray.push(paidImgPath);
     }
   } else if (randomPrompt === prompts[4]) {
     function randomImg(){
       var randomNumber = Math.floor(Math.random() * 10) + 1;
       var imgName = "snickers" + randomNumber;
-      document.getElementById("imageid").src = img/Paid/Snickers + "/" + imgName;
+      var paidImgPath = "img/Paid/Snickers/" + imgName;
+      captchaArray.push(paidImgPath);
     }
   } else if (randomPrompt === prompts[5]) {
     function randomImg(){
       var randomNumber = Math.floor(Math.random() * 10) + 1;
       var imgName = "northface" + randomNumber;
-      document.getElementById("imageid").src = img/Paid/NorthFace + "/" + imgName;
+      var paidImgPath = "img/Paid/NorthFace/" + imgName;
+      captchaArray.push(paidImgPath);
     }
   }
 };
+
+// Combine the image paths into the captchaArray so the order can be shuffled
+
+// Slice the array into segments of 2 so it can be displayed in the 3-row format
