cfd27c64d13952f40d48be5eb7be3c3f2b4248eb
kulikovaaSHU <kulikovaa@sacredheart.edu>
2015-08-11 21:20:54 -0400

    Image button's id passed to alert

--- client/templates/eyecaptcha.js
+++ client/templates/eyecaptcha.js
@@ -3,7 +3,8 @@
 Template.eyecaptchaContainer.events({
     'click .img-button': function(event){
         event.preventDefault();
-	alert(event);
+	//alert(event.target.id);
+	alert(event.target.id);
         //record picked image
     }
 	
@@ -13,6 +14,7 @@ Template.eyecaptchaContainer.events({
 Template.eyecaptchaContainer.events({
 	'click .submit': function(event){
 		event.preventDefault();
+		alert("Submission attempt.")
         //recognise if all chosen images are correct - pass or refresh
 	}
 });
