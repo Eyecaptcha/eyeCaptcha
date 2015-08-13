0c644a8d683b4e09d6245a6d3928b8e3d9754dbc
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-08-12 20:28:22 -0400

    put the entire test inside of a function and made the button clickable to activate the function

--- client/templates/eyecaptcha.js
+++ client/templates/eyecaptcha.js
@@ -19,105 +19,112 @@ Template.eyecaptchaContainer.events({
 	}
 });
 
-// Declare captchaArray variable to be used later
-var captchaArray = [];
+// Wrap all of the EyeCaptcha Test code in a click function
+document.getElementById("generateTest").onclick = EyeCaptchaTest;
 
-// Generate random number function
-function randomIntFromInterval(min,max) {
-   return Math.floor(Math.random()*(max-min+1)+min);
-}
+function EyeCaptchaTest(){
 
-// adImages variable calls randomIntFromInterval function with values 1, 6
-var adImages = randomIntFromInterval(1,6);
+  // Declare captchaArray variable to be used later
+  var captchaArray = [];
 
-// Declare limit of images to 6, calculate # of freeImages
-var captchaLimit = 6;
-var freeImages = captchaLimit - adImages;
-
-// Generate the correct amount of random images from the free images
-for( i = 0; i < freeImages; i++){
-  function randomImg(){
-    var randomNumber = Math.floor(Math.random() * 149) + 1;
-    var imgName = "img" + randomNumber;
-    var imgPath = "img/Free/" + imgName;
-    captchaArray.push(imgPath);
-  }
-};
-
-// Randomly select a prompt to choose from
-var prompts = ["Select the images of the sustainably grown artisanal bouquets now available from Bouqs.",
-"Select the images of the soft drink filled with happiness, now available for only $0.99.",
-"JUST DO IT! Select the images of the footwear brand that gives you the WILL TO WIN!",
-"Select the images of the backpack that can take you anywhere, now available from Patagonia.",
-"Hungry? Select the images of the candy bar that satisfies your craving.",
-"Never stop exploring. Select the images of the jacket that you can take on any expedition, now available from North Face."]
-
-var randomPrompt = prompts[Math.floor(Math.random() * prompts.length)];
-
-// Generate the correct amount of random images from the paid images
-for( i = 0; i < adImages; i++){
-  if(randomPrompt === prompts[0]) {
-    function randomImg(){
-      var randomNumber = Math.floor(Math.random() * 11) + 1;
-      var imgName = "bouqs" + randomNumber;
-      var paidImgPath = "img/Paid/Bouqs/" + imgName;
-    }
-  } else if (randomPrompt === prompts[1]) {
-    function randomImg(){
-      var randomNumber = Math.floor(Math.random() * 10) + 1;
-      var imgName = "coke" + randomNumber;
-      var paidImgPath = "img/Paid/CocaCola/" + imgName;
-    }
-  } else if (randomPrompt === prompts[2]) {
-    function randomImg(){
-      var randomNumber = Math.floor(Math.random() * 10) + 1;
-      var imgName = "nike" + randomNumber;
-      var paidImgPath = "img/Paid/Nike/" + imgName;
-    }
-  } else if (randomPrompt === prompts[3]) {
-    function randomImg(){
-      var randomNumber = Math.floor(Math.random() * 10) + 1;
-      var imgName = "patagonia" + randomNumber;
-      var paidImgPath = "img/Paid/Patagonia/" + imgName;
-    }
-  } else if (randomPrompt === prompts[4]) {
-    function randomImg(){
-      var randomNumber = Math.floor(Math.random() * 10) + 1;
-      var imgName = "snickers" + randomNumber;
-      var paidImgPath = "img/Paid/Snickers/" + imgName;
-    }
-  } else if (randomPrompt === prompts[5]) {
-    function randomImg(){
-      var randomNumber = Math.floor(Math.random() * 10) + 1;
-      var imgName = "northface" + randomNumber;
-      var paidImgPath = "img/Paid/NorthFace/" + imgName;
-    }
-  }
-  captchaArray.push(paidImgPath);
-};
-
-// Shuffle the order of captchaArray
-function shuffle(captchaArray) {
-  var m = captchaArray.length, t, i;
-
-  // While there remain elements to shuffle…
-  while (m) {
-
-    // Pick a remaining element…
-    i = Math.floor(Math.random() * m--);
-
-    // And swap it with the current element.
-    t = captchaArray[m];
-    captchaArray[m] = captchaArray[i];
-    captchaArray[i] = t;
+  // Generate random number function
+  function randomIntFromInterval(min,max) {
+     return Math.floor(Math.random()*(max-min+1)+min);
   }
 
-  return captchaArray;
-}
+  // adImages variable calls randomIntFromInterval function with values 1, 6
+  var adImages = randomIntFromInterval(1,6);
 
-// Slice the array into segments of 2 so it can be displayed in the 3-row format
-var i, j, captchaRows, chunk = 2;
+  // Declare limit of images to 6, calculate # of freeImages
+  var captchaLimit = 6;
+  var freeImages = captchaLimit - adImages;
 
-for (i = 0, j = captchaArray.length; i <= j; i += chunk) {
-   captchaRows = captchaArray.slice ( i, i+chunk );
+  // Generate the correct amount of random images from the free images
+  for( i = 0; i < freeImages; i++){
+    function randomImg(){
+      var randomNumber = Math.floor(Math.random() * 149) + 1;
+      var imgName = "img" + randomNumber;
+      var imgPath = "img/Free/" + imgName;
+      captchaArray.push(imgPath);
+    }
+  };
+
+  // Randomly select a prompt to choose from
+  var prompts = ["Select the images of the sustainably grown artisanal bouquets now available from Bouqs.",
+  "Select the images of the soft drink filled with happiness, now available for only $0.99.",
+  "JUST DO IT! Select the images of the footwear brand that gives you the WILL TO WIN!",
+  "Select the images of the backpack that can take you anywhere, now available from Patagonia.",
+  "Hungry? Select the images of the candy bar that satisfies your craving.",
+  "Never stop exploring. Select the images of the jacket that you can take on any expedition, now available from North Face."]
+
+  var randomPrompt = prompts[Math.floor(Math.random() * prompts.length)];
+
+  // Generate the correct amount of random images from the paid images
+  for( i = 0; i < adImages; i++){
+    if(randomPrompt === prompts[0]) {
+      function randomImg(){
+        var randomNumber = Math.floor(Math.random() * 11) + 1;
+        var imgName = "bouqs" + randomNumber;
+        var paidImgPath = "img/Paid/Bouqs/" + imgName;
+      }
+    } else if (randomPrompt === prompts[1]) {
+      function randomImg(){
+        var randomNumber = Math.floor(Math.random() * 10) + 1;
+        var imgName = "coke" + randomNumber;
+        var paidImgPath = "img/Paid/CocaCola/" + imgName;
+      }
+    } else if (randomPrompt === prompts[2]) {
+      function randomImg(){
+        var randomNumber = Math.floor(Math.random() * 10) + 1;
+        var imgName = "nike" + randomNumber;
+        var paidImgPath = "img/Paid/Nike/" + imgName;
+      }
+    } else if (randomPrompt === prompts[3]) {
+      function randomImg(){
+        var randomNumber = Math.floor(Math.random() * 10) + 1;
+        var imgName = "patagonia" + randomNumber;
+        var paidImgPath = "img/Paid/Patagonia/" + imgName;
+      }
+    } else if (randomPrompt === prompts[4]) {
+      function randomImg(){
+        var randomNumber = Math.floor(Math.random() * 10) + 1;
+        var imgName = "snickers" + randomNumber;
+        var paidImgPath = "img/Paid/Snickers/" + imgName;
+      }
+    } else if (randomPrompt === prompts[5]) {
+      function randomImg(){
+        var randomNumber = Math.floor(Math.random() * 10) + 1;
+        var imgName = "northface" + randomNumber;
+        var paidImgPath = "img/Paid/NorthFace/" + imgName;
+      }
+    }
+    captchaArray.push(paidImgPath);
+  };
+
+  // Shuffle the order of captchaArray - source-code can be found here: http://bost.ocks.org/mike/shuffle/
+  function shuffle(captchaArray) {
+    var m = captchaArray.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = captchaArray[m];
+      captchaArray[m] = captchaArray[i];
+      captchaArray[i] = t;
+    }
+
+    return captchaArray;
+  }
+
+  // Slice the array into segments of 2 so it can be displayed in the 3-row format
+  var i, j, captchaRows, chunk = 2;
+
+  for (i = 0, j = captchaArray.length; i <= j; i += chunk) {
+     captchaRows = captchaArray.slice ( i, i+chunk );
+  }
+
 }
