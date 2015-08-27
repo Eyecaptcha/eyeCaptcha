3c4ff91577e80dab844c0e5a7b699b9c0ca90037
kulikovaaSHU <kulikovaa@sacredheart.edu>
2015-08-27 11:32:30 -0400

    Reformatting code for login.js

--- client/templates/login.js
+++ client/templates/login.js
@@ -1,16 +1,16 @@
 // Login
 
 Template.userLogin.events({
-    'submit form': function(event){
-        event.preventDefault();
-        var email = $('[name=email]').val();
-        var password = $('[name=password]').val();
-        Meteor.loginWithPassword(email, password, function(error){
-          if(error){
-            console.log(error.reason);
-          } else {
-            Router.go("test");
-          }
-        });
-    }
+	'submit form': function(event){
+		event.preventDefault();
+		var email = $('[name=email]').val();
+		var password = $('[name=password]').val();
+		Meteor.loginWithPassword(email, password, function(error){
+			if(error){
+				console.log(error.reason);
+			} else {
+				Router.go("test");
+			}
+		});
+	}
 })
