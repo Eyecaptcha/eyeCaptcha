05faa6e7e82c2e3d8470a616d410a1b784a40904
kulikovaaSHU <kulikovaa@sacredheart.edu>
2015-08-27 11:42:48 -0400

    Reformatting code for register.js

--- client/templates/register.js
+++ client/templates/register.js
@@ -1,12 +1,12 @@
 Template.userRegister.events({
-    'submit form': function(event){
-        event.preventDefault();
-        var email = $('[name=email]').val();
-        var password = $('[name=password]').val();
-        Accounts.createUser({
-            email: email,
-            password: password
-        });
-      Router.go('/test')
-    }
+	'submit form': function(event){
+		event.preventDefault();
+		var email = $('[name=email]').val();
+		var password = $('[name=password]').val();
+		Accounts.createUser({
+			email: email,
+			password: password
+		});
+		Router.go('/test')
+	}
 });
