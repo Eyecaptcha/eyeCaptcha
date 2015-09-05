bc80f3630195cfb616327cab976969e9f66ed9e2
rynjwssl <ryan.wessel@student.fairfield.edu>
2015-09-05 18:41:16 -0400

    working on button press resetting when new test is generated

--- client/templates/eyecaptcha.js
+++ client/templates/eyecaptcha.js
@@ -1,22 +1,34 @@
-// Set variables to start, correct and selected are possibly redundant
-
-	var correct = true;
-
-	// Selected variables to allow select/deselect functionality for image buttons
-	var selected1 = false;
-	var selected2 = false;
-	var selected3 = false;
-	var selected4 = false;
-	var selected5 = false;
-	var selected6 = false;
+// Selected variables to allow select/deselect functionality for image buttons
+var selected1 = 0;
+var selected2 = 0;
+var selected3 = 0;
+var selected4 = 0;
+var selected5 = 0;
+var selected6 = 0;
 
 // Wrap all of the EyeCaptcha Test code in a click function
 Template.eyecaptchaContainer.events({
-	'click #generateTest': function(event){
+	'click #generateTest': function generator(event){
 		event.preventDefault();
 
-    // Reset selectedImages array
+    // Reset selectedImages array and selected variables
     Session.set('selectedImages', []);
+		selected1 = 0;
+		selected2 = 0;
+		selected3 = 0;
+		selected4 = 0;
+		selected5 = 0;
+		selected6 = 0;
+		console.log(selected1);
+
+		if(selected1 % 2 == 0 || selected1 == 0){
+			event.target.style.border = "5px solid #BBDEFB";
+			event.target.style.borderRadius = "5px";
+		}
+		else{
+			event.target.style.border = "5px solid #1E88E5";
+			event.target.style.borderRadius = "5px";
+		}
 
           // Declare captchaArray variable to store image paths in array
           var captchaArray = [];
@@ -39,20 +51,18 @@ Template.eyecaptchaContainer.events({
 
 							// Get count for Free collection
 							var freeImgCount = Free.find().count();
-							console.log(freeImgCount);
 
 							// Generate random number and find freeKey
-							var randomIndex = Math.floor( Math.random() * freeImgCount );
+							var randomIndex = Math.floor( Math.random() * freeImgCount ) + 1;
 							var img = Free.findOne( { freeKey : randomIndex } );
 
 							// Push the key imgPath into captchaArray
               captchaArray.push(img.url());
-							console.log(captchaArray);
           };
 
           // Randomly select a prompt to choose from
 					var testCount = Tests.find().count();
-          var randomTest = Math.floor(Math.random() * testCount);
+          var randomTest = Math.floor(Math.random() * testCount) + 1;
 					var test = Tests.findOne( { testKey : randomTest } );
 					var testPrompt = Tests.findOne( { testKey : randomTest } )['text'];
 
@@ -60,13 +70,11 @@ Template.eyecaptchaContainer.events({
           document.getElementById('promptBox').innerHTML = testPrompt;
 
 					var testImgCount = Tests.findOne( { testKey : randomTest } ).images;
-					console.log(testImgCount);
 
           // Generate the correct amount of random images from the paid images
           for( i = 0; i < adImages; i++){
 						var randomTestImg = Math.floor(Math.random() * testImgCount.length);
             captchaArray.push(testImgCount[randomTestImg]);
-						console.log(captchaArray);
           };
 
           // Shuffle the order of captchaArray - source-code can be found here: http://bost.ocks.org/mike/shuffle/
@@ -83,9 +91,7 @@ Template.eyecaptchaContainer.events({
               captchaArray[m] = captchaArray[i];
               captchaArray[i] = t;
 
-            }
-
-					console.log(captchaArray);
+            };
 
           // Loop through the array to add .src to images and check whether they are free or paid
           for( j = 0, i = 1; i <= captchaLimit; i++, j++ ) {
@@ -94,107 +100,93 @@ Template.eyecaptchaContainer.events({
 
 	},
 
-	'click .paidImg': function(event, template) {
+	'click .images': function(event, template) {
 
-		var selectedImageId = event.target.id
+		var selectedImageId = event.target.id;
 		var selectedImages = Session.get('selectedImages');
 
 		if (selectedImages.indexOf(selectedImageId) == -1) {
-			selectedImages.push(event.target.id);
+			selectedImages.push(event.target.src);
 		}
 
 		Session.set('selectedImages', selectedImages);
 		console.log(Session.get('selectedImages'));
 	},
 
-	'click .freeImg': function(event) {
-
-		correct=false;
-		console.log('Free image clicked');
-	},
-
-	//Selecting/Unselecting images
 	'click #imageid1': function(event) {
-		if(selected1==false){
-			selected1=true;
-			event.target.style.border = "5px solid #1E88E5";
-			event.target.style.borderRadius = "5px";
-		}
-		else{
-			selected1=false;
-			event.target.style.border = "5px solid #BBDEFB";
-			event.target.style.borderRadius = "5px";
-		}
+		selected1 += 1;
+		console.log(selected1);
 	},
 	'click #imageid2': function(event) {
-		if(selected2==false){
-			selected2=true;
-			event.target.style.border = "5px solid #1E88E5";
+		selected2 += 1;
+		if(selected2 % 2 == 0 || selected2 == 0){
+			event.target.style.border = "5px solid #BBDEFB";
 			event.target.style.borderRadius = "5px";
 		}
 		else{
-			selected2=false;
-			event.target.style.border = "5px solid #BBDEFB";
+			event.target.style.border = "5px solid #1E88E5";
 			event.target.style.borderRadius = "5px";
 		}
 	},
 	'click #imageid3': function(event) {
-		if(selected3==false){
-			selected3=true;
-			event.target.style.border = "5px solid #1E88E5";
+		selected3 += 1;
+		if(selected3 % 2 == 0 || selected3 == 0){
+			event.target.style.border = "5px solid #BBDEFB";
 			event.target.style.borderRadius = "5px";
 		}
 		else{
-			selected3=false;
-			event.target.style.border = "5px solid #BBDEFB";
+			event.target.style.border = "5px solid #1E88E5";
 			event.target.style.borderRadius = "5px";
 		}
 	},
 	'click #imageid4': function(event) {
-		if(selected4==false){
-			selected4=true;
-			event.target.style.border = "5px solid #1E88E5";
+		selected4 += 1;
+		if(selected4 % 2 == 0 || selected4 == 0){
+			event.target.style.border = "5px solid #BBDEFB";
 			event.target.style.borderRadius = "5px";
 		}
 		else{
-			selected4=false;
-			event.target.style.border = "5px solid #BBDEFB";
+			event.target.style.border = "5px solid #1E88E5";
 			event.target.style.borderRadius = "5px";
 		}
 	},
 	'click #imageid5': function(event) {
-		if(selected5==false){
-			selected5=true;
-			event.target.style.border = "5px solid #1E88E5";
+		selected5 += 1;
+		if(selected5 % 2 == 0 || selected5 == 0){
+			event.target.style.border = "5px solid #BBDEFB";
 			event.target.style.borderRadius = "5px";
 		}
 		else{
-			selected5=false;
-			event.target.style.border = "5px solid #BBDEFB";
+			event.target.style.border = "5px solid #1E88E5";
 			event.target.style.borderRadius = "5px";
 		}
 	},
 	'click #imageid6': function(event) {
-		if(selected6==false){
-			selected6=true;
-			event.target.style.border = "5px solid #1E88E5";
-			event.target.style.borderRadius = "5px";
-		}
-		else{
-			selected6=false;
+		selected6 += 1;
+		if(selected6 % 2 == 0 || selected6 == 0){
 			event.target.style.border = "5px solid #BBDEFB";
 			event.target.style.borderRadius = "5px";
 		}
-	},
-
+		else{
+			event.target.style.border = "5px solid #1E88E5";
+			event.target.style.borderRadius = "5px";
+		}
+	}
 });
 
-
+Template.img11.events( function() {
+	if(selected1 % 2 == 0 || selected1 == 0){
+		event.target.style.border = "5px solid #BBDEFB";
+		event.target.style.borderRadius = "5px";
+	}
+	else{
+		event.target.style.border = "5px solid #1E88E5";
+		event.target.style.borderRadius = "5px";
+	}
+});
 Template.eyecaptchaContainer.events({
 	'click #submitTest': function(event){
 		var checkCount = Session.get('selectedImages');
-		// console.log(checkCount);
-		// console.log(adCount);
 		if ((checkCount.length == adCount) && (correct == true)) {
 			console.log("SUCCESS!");
 			alert("The test was passed.");
